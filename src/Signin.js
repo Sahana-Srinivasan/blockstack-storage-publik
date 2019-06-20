ffbaba2e85a93821be2dacfae7917b73c4b38b9e
Sahana Srinivasan <sahanas009@gmail.com>
2019-06-20 16:08:00 -0400

    appveyor config

--- src/Signin.js
+++ src/Signin.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Signin extends Component {
-  constructor(props) {
-    super(props);
-  }
+
 
   render() {
     const { handleSignIn } = this.props;
